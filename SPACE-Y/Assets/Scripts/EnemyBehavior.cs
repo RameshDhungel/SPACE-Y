fdc0fc2737b2bde51ea369fbbcbe3414e0c5f1ad
Ramesh Dhungel <dggd>
2019-11-04 23:23:54 -0500

    Creates an Enemy behavior script that has a method to shoot player

---
+++ SPACE-Y/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehavior : MonoBehaviour
+{
+    private Vector2 enemyShootRange = new Vector2(6, 6);
+    float waitTime = 3.0f;
+    public GameObject bulletPrefab;
+    private Transform player;
+    GameObject previousBullet = null;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         if (Mathf.Abs(player.position.x - transform.position.x) < enemyShootRange.x || Mathf.Abs(player.position.y - transform.position.y) < enemyShootRange.y)
+         {
+            Shoot();
+         } 
+    }
+
+    public void Shoot()
+    {
+        if (waitTime < Time.time)
+        {
+            if (previousBullet != null)
+            {
+                Destroy(previousBullet);
+            }
+            GameObject bullet = Instantiate(bulletPrefab, this.gameObject.GetComponentInChildren<Transform>());
+            Vector2 direction = new Vector2(player.transform.position.x, player.transform.position.y);
+            direction.Normalize();
+
+            float rotateAmount = Vector3.Cross(direction, bullet.transform.up).z;
+            bullet.GetComponent<Rigidbody2D>().angularVelocity = -rotateAmount * 1000f;
+            waitTime = Time.time + 1.5f;
+            bullet.GetComponent<Rigidbody2D>().velocity = transform.up = direction * 3000 * Time.fixedDeltaTime;
+            previousBullet = bullet;
+        }
+    }
+}
--- SPACE-Y/Assets/Scripts/EnemyMovement.cs
+++ SPACE-Y/Assets/Scripts/EnemyMovement.cs
@@ -10,10 +10,7 @@ public class EnemyMovement : MonoBehaviour
     private Rigidbody2D enemyRigidbody;
     private Vector2 moveDirection;
     private Vector2 enemyDistance = new Vector2(5,5);
-    private Vector2 enemyShootRange = new Vector2(6, 6);
-    public GameObject bulletPrefab;
     private float rotateAngle;
-    float waitTime = 3.0f;
 
     void Start()
     {
@@ -50,22 +47,5 @@ public class EnemyMovement : MonoBehaviour
     public void MoveEnemy(Vector2 direction)
     {
         enemyRigidbody.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
-    }
-
-     public void Shoot()
-    {
-        if (waitTime < Time.time)
-        {
-            GameObject bullet = Instantiate(bulletPrefab, this.gameObject.GetComponentInChildren<Transform>());
-            Vector2 direction = new Vector2(player.transform.position.x, player.transform.position.y);
-            direction.Normalize();
-
-            float rotateAmount = Vector3.Cross(direction, bullet.transform.up).z;
-            enemyRigidbody.angularVelocity = -rotateAmount * 1000f;
-            // bullet.GetComponent<Rigidbody2D>().velocity = new Vector3 (10,0,rotateAngle);
-            //bullet.GetComponent<Rigidbody2D>().rotation = rotateAngle;
-            waitTime = Time.time + 3.0f;
-            enemyRigidbody.velocity = transform.up = direction * 100 * Time.fixedDeltaTime;
-        }
     }
 }
