93ae7a970bee261ee90f39e6bfb4aa72e782ba0a
abimael <abibarre.82@gmail.com>
2019-11-09 11:46:13 -0500

    yeet

---
+++ SPACE-Y/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    public GameObject Planet;
+    public GameObject PlayerPlaceholder;
+
+
+    public float speed = 4;
+    public float JumpHeight = 1.2f;
+
+    float gravity = 100;
+    bool OnGround = false;
+
+
+    float distanceToGround;
+    Vector3 Groundnormal;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); 
+    }
+    void Update()
+    {
+        //GroundControl
+
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, -transform.up, out hit, 10))
+        {
+
+            distanceToGround = hit.distance;
+            Groundnormal = hit.normal;
+
+            if (distanceToGround <= 0.2f)
+            {
+                OnGround = true;
+            }
+            else
+            {
+                OnGround = false;
+            }
+
+
+        }
+
+
+        //GRAVITY and ROTATION
+
+        Vector3 gravDirection = (transform.position - Planet.transform.position).normalized;
+
+        if (OnGround == false)
+        {
+            rb2d.AddForce(gravDirection * -gravity);
+
+        }
+
+        //
+
+        Quaternion toRotation = Quaternion.FromToRotation(transform.up, Groundnormal) * transform.rotation;
+        transform.rotation = toRotation;
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKey("d") || Input.GetKey("right"))
+        {
+            rb2d.velocity = new Vector2(2, rb2d.velocity.y);
+        }
+        else if (Input.GetKey("a") || Input.GetKey("left"))
+        {
+            rb2d.velocity = new Vector2(-2, rb2d.velocity.y);
+        }
+
+    }
+}
